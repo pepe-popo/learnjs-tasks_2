ea15713e97bb15857954ffb4f11bf3274a3ccd41
pepe-popo <104313537+pepe-popo@users.noreply.github.com>
2023-03-09 00:08:21 +0300

    Все доделал

---
+++ section_11/article_11.2/task_2.js
@@ -0,0 +1,38 @@
+/*Перепишите функцию showCircle, написанную в задании Анимация круга с помощью колбэка таким образом, чтобы она возвращала промис, вместо того чтобы принимать в аргументы функцию-callback.
+
+Новое использование:
+
+showCircle(150, 150, 100).then(div => {
+  div.classList.add('message-ball');
+  div.append("Hello, world!");
+});*/
+
+function go() {
+    showCircle(150, 150, 100)
+        .then(div => {
+            div.classList.add('message-ball');
+            div.append("Привет, мир!");
+        })
+}
+
+function showCircle(cx, cy, radius) {
+    return new Promise((resolve, reject) => {
+        let div = document.createElement('div');
+        div.style.width = 0;
+        div.style.height = 0;
+        div.style.left = cx + 'px';
+        div.style.top = cy + 'px';
+        div.className = 'circle';
+        document.body.append(div);
+
+        setTimeout(() => {
+            div.style.width = radius * 2 + 'px';
+            div.style.height = radius * 2 + 'px';
+
+            div.addEventListener('transitionend', function handler() {
+                div.removeEventListener('transitionend', handler);
+                resolve(div);
+            });
+        }, 0);
+    })
+}
